86e7abb1b2b3bc3bc9f823958f1a49c8d943b1db
Chris Tack <chris_tack@mac.com>
2020-03-09 08:26:10 -0700

    Remove unused code

--- index.test.js
+++ index.test.js
@@ -19,10 +19,6 @@ test('The forEach function', () => {
     });
 
     assert.strictEqual(sum, 6, 'Expected forEach to sum the array')
-
-    // if (sum !== 6) {
-    //     throw new Error('Expected summing array to equal 6');
-    // }
 });
 
 test('The map function', () => {
@@ -31,19 +27,4 @@ test('The map function', () => {
     });
     
     assert.deepStrictEqual(result, [2, 4, 6])
-    // assert.strictEqual(result[0], 2)
-    // assert.strictEqual(result[1], 4)
-    // assert.strictEqual(result[2], 6)
-
-    // if (result[0] !== 2) {
-    //     throw new Error(`Expected to find 2, but found ${result[0]}`)
-    // }
-    
-    // if (result[1] !== 4) {
-    //     throw new Error(`Expected to find 4, but found ${result[1]}`)
-    // }
-    
-    // if (result[2] !== 6) {
-    //     throw new Error(`Expected to find 6, but found ${result[2]}`)
-    // }
 });
